18fff3b5924a9fd4e471eab601951454688e15c7
Bind Pratap singh <158027240+bindpratapsingh@users.noreply.github.com>
2025-09-28 21:47:44 +0530

    Update trafficdataprovider.tsx

--- frontend/src/components/trafficdataprovider.tsx
+++ frontend/src/components/trafficdataprovider.tsx
@@ -57,6 +57,7 @@ const TrafficDataContext = createContext<TrafficDataContextType | undefined>(und
 //const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/dashboard";
 //const WEBSOCKET_URL = "wss://backend-production-039d.up.railway.app/ws/dashboard";
 // Latest : const WEBSOCKET_URL = "wss://backend-production-039d.up.railway.app/ws/ai";
+//const WEBSOCKET_URL = "wss://backend-production-039d.up.railway.app/ws/dashboard";
 const WEBSOCKET_URL = "wss://backend-production-039d.up.railway.app/ws/dashboard";
 
 export const TrafficDataProvider = ({ children }: { children: ReactNode }) => {
