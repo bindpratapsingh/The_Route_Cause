92d496390672090385b95048749f63b292b4bfde
Bind Pratap singh <158027240+bindpratapsingh@users.noreply.github.com>
2025-09-28 17:38:25 +0530

    Update trafficdataprovider.tsx

--- frontend/src/components/trafficdataprovider.tsx
+++ frontend/src/components/trafficdataprovider.tsx
@@ -41,8 +41,8 @@ const TrafficDataContext = createContext<TrafficDataContextType | undefined>(und
 // Define the WebSocket URL
 //const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/dashboard";
 //const socket = new WebSocket("wss://backend-production-039d.up.railway.app");
-const WEBSOCKET_URL = "ws://backend-production-039d.up.railway.app/ws/dashboard";
-//const WEBSOCKET_URL = process.env.VITE_WEBSOCKET_URL || "ws://localhost:8000/ws/dashboard";
+//const WEBSOCKET_URL = "ws://backend-production-039d.up.railway.app/ws/dashboard";
+const WEBSOCKET_URL = process.env.VITE_WEBSOCKET_URL || "backend-production-039d.up.railway.app";
 // --- Create the Provider Component ---
 // This component will wrap your entire application. It handles the actual
 // connection and provides the data to all children.
