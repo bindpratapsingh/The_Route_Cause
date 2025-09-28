4f9f83e5c315f68d871575f2704f0b83a0dbd5d1
Bind Pratap singh <158027240+bindpratapsingh@users.noreply.github.com>
2025-09-28 17:41:42 +0530

    Update trafficdataprovider.tsx

--- frontend/src/components/trafficdataprovider.tsx
+++ frontend/src/components/trafficdataprovider.tsx
@@ -39,7 +39,9 @@ interface TrafficDataContextType {
 const TrafficDataContext = createContext<TrafficDataContextType | undefined>(undefined);
 
 // Define the WebSocket URL
-const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/dashboard";
+//const WEBSOCKET_URL = "ws://127.0.0.1:8000/ws/dashboard";
+const WEBSOCKET_URL = "wss://backend-production-039d.up.railway.app/ws/dashboard";
+
 //const socket = new WebSocket("wss://backend-production-039d.up.railway.app");
 //const WEBSOCKET_URL = "ws://backend-production-039d.up.railway.app/ws/dashboard";
 //const WEBSOCKET_URL = process.env.VITE_WEBSOCKET_URL || "backend-production-039d.up.railway.app";
